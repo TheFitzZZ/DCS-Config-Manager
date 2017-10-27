f405761825dd07ee43aed98ae0ec1fc572091e2f
Christoph <fitzzz@fitzzz.de>
2017-10-27 19:59:52 +0200

    Publish test

--- DCS-ConfigMgmt/Properties/AssemblyInfo.cs
+++ DCS-ConfigMgmt/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("DCS-ConfigMgmt")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DCS Configuration Manger")]
+[assembly: AssemblyDescription("Helps managing control and VR configuration for DCS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DCS-ConfigMgmt")]
+[assembly: AssemblyCompany("924th vFW // FitzZZ")]
+[assembly: AssemblyProduct("DCS Configuration Manger")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
